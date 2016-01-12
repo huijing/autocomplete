55c47e704f065610026f7f4b7c7d5275c53c1514
HuiJing Chen <huijng.chen@publicis.com.sg>
2016-01-12 19:20:04 +0800

    Refactor scripts

--- scripts.js
+++ scripts.js
@@ -6,7 +6,9 @@ void 0===c?d&&"get"in d&&null!==(e=d.get(a,b))?e:(e=n.find.attr(a,b),null==e?voi
 var shows;
 var showTitles = [];
 
-function load_lookup () {
+function load_lookup (callback) {
+    showTitles = [];
+
     $.ajax({
         url: 'https://api.viki.io/v4/search.json?c=b&per_page=5&with_people=true&with_paywall=1&app=100266a&t=1440586215',
     })
@@ -15,8 +17,15 @@ function load_lookup () {
         for (var i = 0; i < shows.length; i++) {
             showTitles.push(shows[i].tt);
         }
-        console.log(showTitles);
-        init_lookup();
+
+        // test if complete is a function
+        // if (typeof(complete) === function) {
+        // }
+
+
+    })
+    .complete(function () {
+        callback.complete()    
     })
     .fail(function() {
         console.log("Data not loaded");
@@ -26,48 +35,51 @@ function load_lookup () {
 function init_lookup () {
     var search_field = $('#searchField');
     var filtered_shows = [];
-    var cache = {};
     var createList = false;
-
+   
 
     search_field.on({
         keyup: function () {
             var search_val = $(this).val(); 
             var form = $('form');
             var results;
+            var resultsList = $('#resultsList');
+            resultsList.html('');
 
-            if (search_val in cache) {
-                results = cache[search_val];
-            } else {
-                results = $.grep(showTitles, function(item) {
-                    return item.search(RegExp(search_val, "i")) != -1;
-                });
-            }
+            // load my lookup via ajax
+            load_lookup({
 
-            if (createList === false) {
-                var list = $("<ul>", {
-                    id: 'resultsList',
-                    class: 'results-list'
-                });
-                form.after(list);
-                createList = true;
-                $('#resultsList').on("click", "li", function(){
-                    search_field.val($(this).text());
-                    $('#resultsList').empty();
-                });
-            } else {
-                $("#resultsList").empty();
-            }
+                complete: function() {
+                    var results = showTitles.filter(function(item) {
+                        if (item.indexOf(search_val) != -1) {
+                            return true;
+                        }
+                        return item.search(RegExp(search_val, "i")) != -1;
+                    });
 
-            for(var term in results){
-                $("#resultsList").append("<li>" + results[term] + "</li>");
-            }
+                    if (createList === false) {
+                        resultsList = $("<ul>", {
+                            id: 'resultsList',
+                            class: 'results-list'
+                        });
+                        form.after(resultsList);
+                        createList = true;
+                        resultsList.on("click", "li", function(){
+                            search_field.val($(this).text());
+                        });
+                    }
 
-            if (search_val.length === 0) {
-                $("#resultsList").empty();
-            }
+                    for(var term in results){
+                        resultsList.append("<li>" + results[term] + "</li>");
+                    }
+
+                    if (search_val.length === 0) {
+                        resultsList.html('');
+                    }
+                }
+            });
         },
     });
 }
 
-load_lookup();
+init_lookup();
