bb46cf92bb322bd772daf84912ac353583369e65
huijing <kakyou_tensai@yahoo.com>
2016-01-12 23:52:53 +0800

    Clean up test code and comments

--- scripts.js
+++ scripts.js
@@ -17,15 +17,9 @@ function load_lookup (callback) {
         for (var i = 0; i < shows.length; i++) {
             showTitles.push(shows[i].tt);
         }
-
-        // test if complete is a function
-        // if (typeof(complete) === function) {
-        // }
-
-
     })
     .complete(function () {
-        callback.complete()    
+        callback.complete();  
     })
     .fail(function() {
         console.log("Data not loaded");
@@ -37,18 +31,14 @@ function init_lookup () {
     var filtered_shows = [];
     var createList = false;
    
-
     search_field.on({
         keyup: function () {
             var search_val = $(this).val(); 
-            var form = $('form');
             var results;
             var resultsList = $('#resultsList');
             resultsList.html('');
 
-            // load my lookup via ajax
             load_lookup({
-
                 complete: function() {
                     var results = showTitles.filter(function(item) {
                         if (item.indexOf(search_val) != -1) {
@@ -57,17 +47,10 @@ function init_lookup () {
                         return item.search(RegExp(search_val, "i")) != -1;
                     });
 
-                    if (createList === false) {
-                        resultsList = $("<ul>", {
-                            id: 'resultsList',
-                            class: 'results-list'
-                        });
-                        form.after(resultsList);
-                        createList = true;
-                        resultsList.on("click", "li", function(){
-                            search_field.val($(this).text());
-                        });
-                    }
+                    resultsList.on("click", "li", function(){
+                        search_field.val($(this).text());
+                        resultsList.html('');
+                    });
 
                     for(var term in results){
                         resultsList.append("<li>" + results[term] + "</li>");
